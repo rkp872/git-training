fb6279ffbf0de43859f323fc08a60dd1a507d203
rohit_pandey <rohit_pandey@persistent.com>
2021-01-25 14:59:01 +0530

    Added math.py

---
+++ math.py
@@ -0,0 +1,25 @@
+# Perform addition
+def add(x,y):
+    return x+y
+# Perform subtraction
+def subtract(x,y):
+    if x<y :
+        return "ERROR"
+    return x-y
+# Perform multiplication
+def multiply(x,y):
+    return x*y
+    
+# Perform division
+def divide(x,y):
+    if( y==0) :
+        return "Divide by 0 message"
+<<<<<<< HEAD
+    return x/y
+=======
+>>>>>>> BugFix02
+return x/y
+
+def square(x):
+    return x*x;
+>>>>>>> 2a5cef8 (Square fn implemention)
